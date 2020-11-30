0aa04185ec2b13b393b122a02d655f0b8f678998
unknown <quinnrodrigues98@gmail.com>
2020-11-30 12:50:19 -0500

    Updated the Project Facebook-Fake

--- Project-Z/Project_fakeFaceBook/script.js
+++ Project-Z/Project_fakeFaceBook/script.js
@@ -1,7 +1,15 @@
 var database = [{
-    username: "Winnie",
-    password: "passWord10020",
-}];
+        username: "Winnie",
+        password: "passWord10020",
+    } {
+        username: "Tigger",
+        password: "passWord10021",
+    } {
+        username: "Piglet",
+        password: "passWord10022",
+    }
+
+];
 
 var newsFeed = [{
         username: "Christopher",
@@ -9,14 +17,27 @@ var newsFeed = [{
     },
     {
         username: "Eeyore",
-        timeline: "Javasript is sooooo cool!"
+        timeline: "Need Lumber jack to help with logs"
+    }
+
+    {
+        username: "Rabbit",
+        timeline: "100 Acre Woods is having a sale!"
     }
 ];
 
-var userNamePrompt = prompt(" What's your name?");
-var passWordPrompt = prompt("What's your password?");
 
-function signIn(user, pass) {
+function isUserValid(username, password) {
+    for (valid i = 0; i < database.length; i++) {
+        if (database[i].username === username && database[i].password === password) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function signIn(username, password) {
     if (user === database[0].username && pass === database[0].password) {
         console.log(newsFeed);
 
@@ -25,5 +46,9 @@ function signIn(user, pass) {
     }
 
 }
+
+var userNamePrompt = prompt(" What\'s your name?");
+var passWordPrompt = prompt("What\'s your password?");
+
 
 signIn(userNamePrompt, passWordPrompt); //calling the function with 2 arguments to fit the function parameters
